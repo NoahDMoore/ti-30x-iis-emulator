2ee38f18494feae3e91c0f005ba836d4e501e6d4
NoahDMoore <70867820+NoahDMoore@users.noreply.github.com>
2026-06-23 10:17:03 -0500

    Add additional function tokens

--- src/js/calculator/tokens/function-tokens.js
+++ src/js/calculator/tokens/function-tokens.js
@@ -31,6 +31,13 @@ export default {
         arity: 1
     },
 
+    "scientificNotation": {
+        id: "scientificNotation",
+        glyphsIDs: ["E"],
+        type: "function",
+        arity: 2
+    },
+
     "nPr": {
         id: "nPr",
         glyphIDs: ["space", "n", "P", "r", "space"],
@@ -43,6 +50,13 @@ export default {
         glyphIDs: ["space", "n", "C", "r", "space"],
         type: "function",
         arity: 2
+    },
+
+    "factorial": {
+        id: "factorial",
+        glyphIDs: ["!"],
+        type: "function",
+        arity: 1
     },
 
     "rand": {
@@ -106,5 +120,91 @@ export default {
         glyphIDs: ["rightPointer", "A", "simpleFraction", "bidirectionalPointer", "mixedFraction"],
         type: "function",
         arity: 2
-    }
+    },
+
+    "sin": {
+        id: "sin",
+        glyphIDs: ["s", "i", "n", "("],
+        type: "function",
+        arity: 1
+    },
+
+    "inverseSin": {
+        id: "inverseSin",
+        glyphIDs: ["s", "i", "n", "-1", "("],
+        type: "function",
+        arity: 1
+    },
+
+    "cos": {
+        id: "cos",
+        glyphIDs: ["c", "o", "s", "("],
+        type: "function",
+        arity: 1
+    },
+
+    "inverseCos": {
+        id: "inverseCos",
+        glyphIDs: ["c", "o", "s", "-1", "("],
+        type: "function",
+        arity: 1
+    },
+
+    "tan": {
+        id: "tan",
+        glyphIDs: ["t", "a", "n", "("],
+        type: "function",
+        arity: 1
+    },
+
+    "inverseTan": {
+        id: "inverseTan",
+        glyphIDs: ["t", "a", "n", "-1", "("],
+        type: "function",
+        arity: 1
+    },
+
+    "xRoot": {
+        id: "xRoot",
+        glyphIDs: ["xRootIndex", "root"],
+        type: "function",
+        arity: 2
+    },
+
+    "squareRoot": {
+        id: "squareRoot",
+        glyphIDs: ["root", "("],
+        type: "function",
+        arity: 1
+    },
+
+    "square": {
+        id: "square",
+        glyphIDs: ["squared"],
+        type: "function",
+        arity: 1
+    },
+
+    "exponent": {
+        id: "exponent",
+        glyphIDs: ["caret"],
+        type: "function",
+        arity: 2
+    },
+
+    "invert": {
+        id: "invert",
+        glyphIDs: ["-1"],
+        type: "function",
+        arity: 1
+    },
+
+    "percent": {
+        id: "percent",
+        glyphIDs: ["%"],
+        type: "function",
+        arity: 1
+    },
+
+    
 }
